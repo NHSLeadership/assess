b13337a8b9c2ddae3aa7a63c393c7d74d2be41aa
Andrew Blane <andrew.blane@nhs.net>
2025-11-11 14:33:52 +0000

    [refs #DIG-7805] Add policies

---
+++ app/Policies/FrameworkVariantAttributePolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\FrameworkVariantAttribute;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class FrameworkVariantAttributePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, FrameworkVariantAttribute $frameworkVariantAttribute): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, FrameworkVariantAttribute $frameworkVariantAttribute): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, FrameworkVariantAttribute $frameworkVariantAttribute): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, FrameworkVariantAttribute $frameworkVariantAttribute): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, FrameworkVariantAttribute $frameworkVariantAttribute): bool
+    {
+        return false;
+    }
+}
