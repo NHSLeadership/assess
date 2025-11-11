9dfdfd01e9534c35f88d6f8252ceb724afff07d1
Andrew Blane <andrew.blane@nhs.net>
2025-11-11 11:29:30 +0000

    [refs #DIG-7805] Add database migrations

---
+++ database/migrations/2025_11_11_105956_create_assessment_variant_selection_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessment_variant_selection', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assessment_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('framework_variant_attribute_id')
+                ->constrained('framework_variant_attributes')->cascadeOnDelete();
+            $table->foreignId('framework_variant_option_id')
+                ->constrained('framework_variant_options')->cascadeOnDelete();
+            $table->timestamps();
+            $table->unique(['assessment_id', 'framework_variant_attribute_id'],
+                'assessment_variant_unique_attr');
+            $table->index(['assessment_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessment_variant_selection');
+    }
+};
