e747bd89c62293ae2ba6cd93f59ed3be08fa0aaf
Mahesh Murali P <maheshmuralip@gmail.com>
2025-11-25 11:47:33 +0000

    Refactor user method to remove commented code
    Cleaned up the user() method in UserTrait by removing commented-out code and streamlining the creation of the user instance.

--- app/Traits/UserTrait.php
+++ app/Traits/UserTrait.php
@@ -8,22 +8,13 @@ trait UserTrait
     #[Computed]
     public function user(): ?User
     {
-//        $user = [
-//            'name' => 'Marcin Calka',
-//            'email' => 'marcin.calka@nhs.net',
-//        ];
         $user = User::firstOrCreate(
-        // Lookup attributes (used to check if the record exists)
             ['email' => 'mahesh.muralipoovampilly@nhs.net'],
-
-            // Values to set if creating a new record
             [
                 'name'     => 'Mahesh Murali Poovampilly',
                 'password' => bcrypt(str()->password()), // always hash passwords
             ]
         );
-
-//        $user->id = 1;
 
         return $user;
     }
