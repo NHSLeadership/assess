2c0b699707f4b6540ade109ec5a6d5f4675cf642
Andrew Blane <andrew.blane@nhs.net>
2025-12-12 10:51:22 +0000

    [refs #DIG-7935] Add framework instructions column

--- database/seeders/FrameworkSeeder.php
+++ database/seeders/FrameworkSeeder.php
@@ -16,6 +16,7 @@ class FrameworkSeeder extends Seeder
             'name'        => 'Management and Leadership framework',
             'slug'        => 'mlf',
             'description' => 'The framework consists of a code of practice, standards and competencies at all levels from entry level manager to executive, and a learning and development curriculum.',
+            'instructions' => '<h2>Welcome to your NHS Management and Leadership assessment</h2><p>In this assessment you will be presented with a series of questions.</p><p>To get the most value from this exercise answer these questions as accurately and honestly as you can.</p>',
         ]);
     }
 }
