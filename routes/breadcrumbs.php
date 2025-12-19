bd7908281a28dbac4f14ddffa9ef389194b94491
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2025-12-19 13:22:03 +0000

    DIG-7952 Removes breadcrumb for timebeing

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,30 +2,30 @@
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
 // Home
-Breadcrumbs::for('home', function ($trail) {
-    $trail->push('Home', route('home'));
-});
+//Breadcrumbs::for('home', function ($trail) {
+//    $trail->push('Home', route('home'));
+//});
 
 // Home > Summary
-Breadcrumbs::for('summary', function ($trail) {
-    $trail->parent('frameworks');
-    $trail->push('Summary', route('summary'));
-});
+//Breadcrumbs::for('summary', function ($trail) {
+//    $trail->parent('frameworks');
+//    $trail->push('Summary', route('summary'));
+//});
 
 // Home > Assessments
-Breadcrumbs::for('assessments', function ($trail) {
-    $trail->parent('frameworks');
-    $trail->push('Assessments', route('assessments'));
-});
+//Breadcrumbs::for('assessments', function ($trail) {
+//    $trail->parent('frameworks');
+//    $trail->push('Assessments', route('assessments'));
+//});
 
 // Home > Frameworks
-Breadcrumbs::for('frameworks', function ($trail) {
-    $trail->parent('home');
-    $trail->push('Frameworks', route('frameworks'));
-});
+//Breadcrumbs::for('frameworks', function ($trail) {
+//    $trail->parent('home');
+//    $trail->push('Frameworks', route('frameworks'));
+//});
 
 // Home > Stages
-Breadcrumbs::for('stages', function ($trail) {
-    $trail->parent('home');
-    $trail->push('Standard and Competencies', route('standards'));
-});
+//Breadcrumbs::for('stages', function ($trail) {
+//    $trail->parent('home');
+//    $trail->push('Standard and Competencies', route('standards'));
+//});
