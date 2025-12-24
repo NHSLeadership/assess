0c6dfd9e52449dd648d45b1647665e2dca55800e
Andrew Blane <andrew.blane@nhs.net>
2025-12-24 11:35:47 +0000

    [refs #DIG-7975] Add reorder permission

--- app/Policies/FrameworkVariantOptionPolicy.php
+++ app/Policies/FrameworkVariantOptionPolicy.php
@@ -62,4 +62,12 @@ class FrameworkVariantOptionPolicy
     {
         return true;
     }
+
+    /**
+     * Determine whether records can be reordered in a table.
+     */
+    public function reorder(User $user): bool
+    {
+        return true;
+    }
 }
--- app/Policies/QuestionVariantPolicy.php
+++ app/Policies/QuestionVariantPolicy.php
@@ -63,4 +63,12 @@ class QuestionVariantPolicy
     {
         return true;
     }
+
+    /**
+     * Determine whether records can be reordered in a table.
+     */
+    public function reorder(User $user): bool
+    {
+        return true;
+    }
 }
