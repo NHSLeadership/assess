f05fa5ab8a16ee72bede3b2ea02b295b70ea7c49
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2026-01-15 17:08:17 +0000

    DIG-8016 Remove commented and unused code from assessments view

--- resources/views/livewire/assessments.blade.php
+++ resources/views/livewire/assessments.blade.php
@@ -1,33 +1,6 @@
 <div class="nhsuk-grid-row">
     <div class="nhsuk-grid-column-full">
 
-{{--        @if (!empty($this->assessment->framework->name))--}}
-{{--            <h1 class="nhsuk-heading-xl nhsuk-u-margin-bottom-3">{{ $this->assessment->framework->name }}</h1>--}}
-{{--        @endif--}}
-
-        @if (!empty($this->assessment->id))
-{{--            <div class="nhsuk-action-link disabled">--}}
-{{--                <a class="nhsuk-action-link__link disabled"  href="{{ route('review-request', $this->assessment->id) }}">--}}
-{{--                    <svg class="nhsuk-icon nhsuk-icon__arrow-right-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">--}}
-{{--                        <path d="M0 0h24v24H0z" fill="none"></path>--}}
-{{--                        <path d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"></path>--}}
-{{--                    </svg>--}}
-{{--                    <span class="nhsuk-action-link__text">{{ __('Request 360 Review') }}</span>--}}
-{{--                </a>--}}
-{{--            </div>--}}
-        @endif
-        <style>
-            .test-headers {
-                /*display: inline-block;          */
-                /*background: #e0f0ff;    */
-                /*padding: 0.25em 0.5em;   */
-                /*margin: 0;              */
-                display: inline-block;
-                background: #e0f0ff;
-                padding: 0.25em 0.5em;
-                margin: 0;
-            }
-        </style>
         @if ($this->nodes?->count())
 
             @foreach ($paginatedNodes as $node)
@@ -37,10 +10,8 @@
                         <div>
                             <{{ $item['headingTag'] }} class="{{ $item['headingClass'] }} nhsuk-u-padding-2 nhsuk-u-display-inline-block nhsuk-u-margin-top-0 nhsuk-u-margin-bottom-0" style="background-color: {{ \App\Enums\NodeColour::from($item['colour'])?->hex() ?? 'red' }};">
                             @if(!empty($item['name']))
-    {{--                            <span class="nhsuk-tag--{{ $item['colour'] }} nhsuk-tag--no-border nhsuk-u-padding-2 nhsuk-u-display-inline-block">--}}
                                     {{ config('app.show_node_type_prefix') && !empty($item['type']) ? $item['type'] . ': ' : '' }}
                                     {{ $item['name'] }}
-    {{--                            </span>--}}
                             @endif
                         </{{ $item['headingTag'] }}>
                     </div>
