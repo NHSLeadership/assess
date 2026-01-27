92ef90d42b6f96426ef8e2760ec67417271f7d69
Andrew Blane <andrew.blane@nhs.net>
2026-01-27 11:36:40 +0000

    [refs #DIG-8080] Create reporting views

---
+++ database/migrations/2026_01_27_094558_create_assessments_view.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('
+            CREATE VIEW assessments_view AS
+                SELECT
+                    a.id AS assessment_id,
+                    f.name AS framework_name,
+                    a.user_id,
+                    fva.key AS attribute_key,
+                    fvo.value AS option_value,
+                    a.created_at,
+                    a.submitted_at
+                FROM
+                    assessments a
+                    JOIN frameworks f ON f.id = a.framework_id
+                    LEFT JOIN assessment_variant_selections avs ON avs.assessment_id = a.id
+                    LEFT JOIN framework_variant_attributes fva ON fva.id = avs.framework_variant_attribute_id
+                    LEFT JOIN framework_variant_options fvo ON fvo.id = avs.framework_variant_option_id
+                ORDER BY
+                    a.id DESC;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("DROP VIEW assessments_view");
+    }
+};
