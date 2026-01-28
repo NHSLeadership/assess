f2a80a15a992d09a69e568f8157157eeb1a428ae
Andrew Blane <andrew.blane@nhs.net>
2026-01-28 09:47:48 +0000

    [refs #DIG-8057] Fix assessment factory

--- database/factories/AssessmentFactory.php
+++ database/factories/AssessmentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Assessment>
@@ -16,8 +17,16 @@ class AssessmentFactory extends Factory
      */
     public function definition(): array
     {
+
+        $minDate = Carbon::parse('2026-01-01');
+        $now     = now();
+
         return [
-            //
+            'user_id' => $this->faker->unique()->numberBetween(1000, 999999),
+            'framework_id' => 1,
+            'submitted_at' => $this->faker->optional(0.6)->dateTimeBetween($minDate, $now),
+            'created_at' => $this->faker->dateTimeBetween($minDate, $now),
+            'updated_at' => $this->faker->dateTimeBetween($minDate, $now),
         ];
     }
 }
