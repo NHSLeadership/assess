b7b5a49cdbecb2a54f6f5f870e7c3e7eb756a5cc
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2026-03-24 13:55:47 +0000

    DIG-8208 Code refactor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         if (! app()->environment(['local', 'development', 'staging'])) {
-            $this->command?->error('Seeding is disabled in production.');
+            $this->command->error('Seeding is disabled in production.');
             Log::warning('Database seeding attempted in production and was skipped.');
 
             return;
