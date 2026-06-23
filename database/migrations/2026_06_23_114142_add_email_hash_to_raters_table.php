10711a3bf35d7df28cde117f75847a2a58be8afe
Andrew Blane <andrew.blane@nhs.net>
2026-06-23 14:37:46 +0100

    [refs #DIG-8431] Regenerate responses_view

--- database/migrations/2026_06_23_114142_add_email_hash_to_raters_table.php
+++ database/migrations/2026_06_23_114142_add_email_hash_to_raters_table.php
@@ -10,6 +10,8 @@ return new class extends Migration
 {
     public function up(): void
     {
+        DB::statement('DROP VIEW IF EXISTS responses_view');
+
         Schema::table('raters', function (Blueprint $table) {
             $table->dropUnique([
                 'subject_id',
@@ -60,10 +62,45 @@ return new class extends Migration
                 'email_hash',
             ]);
         });
+
+        DB::statement('
+            CREATE VIEW responses_view AS
+                SELECT
+                    r.assessment_id,
+                    r.id AS response_id,
+                    rt.subject_id AS rater_user_id,
+                    ar.type AS rater_role,
+                    nt.name AS node_type,
+                    n.name AS node_name,
+                    q.id AS question_id,
+                    q.title AS question_title,
+                    so.value AS score,
+                    so.label AS scale_label,
+                    r.created_at AS response_created_at
+                FROM
+                    responses r
+                    JOIN assessments a ON a.id = r.assessment_id
+                    JOIN questions q ON q.id = r.question_id
+                    JOIN nodes n ON n.id = q.node_id
+                    JOIN node_types nt ON nt.id = n.node_type_id
+                    LEFT JOIN scale_options so ON so.id = r.scale_option_id
+                    LEFT JOIN assessment_rater ar ON ar.assessment_id = r.assessment_id
+                    AND ar.rater_id = r.rater_id
+                    LEFT JOIN raters rt ON rt.id = ar.rater_id
+                WHERE
+                    q.response_type = "scale"
+                ORDER BY
+                    r.assessment_id,
+                    ar.type,
+                    n.`order`,
+                    q.id;
+        ');
     }
 
     public function down(): void
     {
+        DB::statement('DROP VIEW IF EXISTS responses_view');
+
         Schema::table('raters', function (Blueprint $table) {
             $table->dropUnique([
                 'subject_id',
@@ -102,5 +139,38 @@ return new class extends Migration
             ]);
             $table->index('email');
         });
+
+        DB::statement('
+            CREATE VIEW responses_view AS
+                SELECT
+                    r.assessment_id,
+                    r.id AS response_id,
+                    rt.subject_id AS rater_user_id,
+                    ar.type AS rater_role,
+                    nt.name AS node_type,
+                    n.name AS node_name,
+                    q.id AS question_id,
+                    q.title AS question_title,
+                    so.value AS score,
+                    so.label AS scale_label,
+                    r.created_at AS response_created_at
+                FROM
+                    responses r
+                    JOIN assessments a ON a.id = r.assessment_id
+                    JOIN questions q ON q.id = r.question_id
+                    JOIN nodes n ON n.id = q.node_id
+                    JOIN node_types nt ON nt.id = n.node_type_id
+                    LEFT JOIN scale_options so ON so.id = r.scale_option_id
+                    LEFT JOIN assessment_rater ar ON ar.assessment_id = r.assessment_id
+                    AND ar.rater_id = r.rater_id
+                    LEFT JOIN raters rt ON rt.id = ar.rater_id
+                WHERE
+                    q.response_type = "scale"
+                ORDER BY
+                    r.assessment_id,
+                    ar.type,
+                    n.`order`,
+                    q.id;
+        ');
     }
 };
