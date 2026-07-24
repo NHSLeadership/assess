bd2e38964094825e69248a6a2fa98006aab24b01
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2026-07-24 14:44:25 +0100

    assess-flow-with-raters Fix questions back-link routing by user role
    Updates the “Previous page” button logic in `questions.blade.php` when no responses exist. The button is now shown only when there is no `raterId`, and its destination is chosen by permission: users with `assess:360` go to raters, while others return to variant selection.

--- resources/views/livewire/questions.blade.php
+++ resources/views/livewire/questions.blade.php
@@ -81,10 +81,12 @@
                     </div>
                 @else
                     {{-- No responses yet, from the variant select page --}}
-                    @if (!empty($this->raterId))
-                        <button wire:click.prevent="goToRaters()" class="nhsuk-button nhsuk-button--secondary nhsuk-u-margin-right-3">Previous page</button>
-                    @else
-                        <button wire:click.prevent="goToVariantSelection" class="nhsuk-button nhsuk-button--secondary nhsuk-u-margin-right-3">Previous page</button>
+                    @if (empty($this->raterId))
+                        @if ($this->user()?->can('assess:360'))
+                            <button wire:click.prevent="goToRaters()" class="nhsuk-button nhsuk-button--secondary nhsuk-u-margin-right-3">Previous page</button>
+                        @else
+                            <button wire:click.prevent="goToVariantSelection()" class="nhsuk-button nhsuk-button--secondary nhsuk-u-margin-right-3">Previous page</button>
+                        @endif
                     @endif
                     <button class="nhsuk-button" type="submit">Save and continue</button>
                 @endif
