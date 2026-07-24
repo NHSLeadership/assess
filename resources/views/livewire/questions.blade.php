1ce5cfaedb3ddc3164fac74cd179eef8e466c3c9
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2026-07-24 14:12:06 +0100

    assess-flow-with-raters Fix previous page button logic for rater/variant
    Corrected inverted condition: show 'go to raters' button when raterId is set, and add fallback to go to variant selection when no raterId is present.

--- resources/views/livewire/questions.blade.php
+++ resources/views/livewire/questions.blade.php
@@ -81,8 +81,10 @@
                     </div>
                 @else
                     {{-- No responses yet, from the variant select page --}}
-                    @if (empty($this->raterId))
+                    @if (!empty($this->raterId))
                         <button wire:click.prevent="goToRaters()" class="nhsuk-button nhsuk-button--secondary nhsuk-u-margin-right-3">Previous page</button>
+                    @else
+                        <button wire:click.prevent="goToVariantSelection" class="nhsuk-button nhsuk-button--secondary nhsuk-u-margin-right-3">Previous page</button>
                     @endif
                     <button class="nhsuk-button" type="submit">Save and continue</button>
                 @endif
