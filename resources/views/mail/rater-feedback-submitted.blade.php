63349d0ffd9582d6c750487c2f0e82d5c9958853
Andrew Blane <andrew.blane@nhs.net>
2026-08-27 16:17:14 +0100

    [refs #fix-email-links] Remove indentation in markdown

--- resources/views/mail/rater-feedback-submitted.blade.php
+++ resources/views/mail/rater-feedback-submitted.blade.php
@@ -1,14 +1,17 @@
 @component('mail::message')
-    A rater has submitted their feedback for your assessment.
-    <br><br>
-    You can view the progress of your assessment using the button below.
-    <br><br>
-    @component('mail::button', ['url' => route('assessment-raters', ['assessmentId' => $assessment->id])])
-        View assessment
-    @endcomponent
-    <br><br>
-    For further guidance and support visit our <a href="https://support.leadershipacademy.nhs.uk/">support page</a>.<br><br>
-    Best regards,<br><br>
-    Assessment System Team<br><br>
-    W: <a href="https://leadershipacademy.nhs.uk">leadershipacademy.nhs.uk</a> | Follow us: @NHSLeadership
+A rater has submitted their feedback for your assessment.
+
+You can view the progress of your assessment using the button below.
+
+@component('mail::button', ['url' => route('assessment-raters', ['assessmentId' => $assessment->id])])
+View assessment
+@endcomponent
+
+For further guidance and support visit our [support page](https://support.leadershipacademy.nhs.uk/).
+
+Best regards,
+
+Assessment System Team
+
+W: [leadershipacademy.nhs.uk](https://leadershipacademy.nhs.uk) | Follow us: @NHSLeadership
 @endcomponent
