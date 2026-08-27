61a62542829fced5c401939a4850f9a1ba15508e
Andrew Blane <andrew.blane@nhs.net>
2026-08-27 16:49:20 +0100

    [refs #fix-email-links] Make email endings consistent (#474)

--- resources/views/mail/assessment-completed.blade.php
+++ resources/views/mail/assessment-completed.blade.php
@@ -7,11 +7,8 @@ Thank you for completing your assessment.
 
 We appreciate the time and effort you’ve invested in completing this.
 
-For further guidance and support visit our [support page](https://support.leadershipacademy.nhs.uk/).
+For further guidance and support visit our
+[support page](https://support.leadershipacademy.nhs.uk/).
 
-Best regards,
-
-Assessment System Team
-
-W: [leadershipacademy.nhs.uk](https://leadershipacademy.nhs.uk) | Follow us: @NHSLeadership
+NHS Leadership Academy
 @endcomponent
--- resources/views/mail/assessment-expiry-warning.blade.php
+++ resources/views/mail/assessment-expiry-warning.blade.php
@@ -7,12 +7,11 @@ If you still need this assessment, please sign in and choose to keep it.
 If no action is taken, the assessment will be deleted automatically.
 
 @component('mail::button', ['url' => config('app.url')])
-Sign in to Assessment System
+Sign in
 @endcomponent
 
-For further guidance and support visit our [support page](https://support.leadershipacademy.nhs.uk/).
+For further guidance and support visit our
+[support page](https://support.leadershipacademy.nhs.uk/).
 
-Best regards,
-Assessment System Team
-W: [leadershipacademy.nhs.uk](https://leadershipacademy.nhs.uk) | Follow us: @NHSLeadership
+NHS Leadership Academy
 @endcomponent
