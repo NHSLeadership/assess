d1ce8199371981949923ec786b6c1139aa25af5c
Andrew Blane <andrew.blane@nhs.net>
2026-08-27 15:48:51 +0100

    [refs #DIG-8544] Use HTML links in emails

--- resources/views/mail/assessment-completed.blade.php
+++ resources/views/mail/assessment-completed.blade.php
@@ -5,8 +5,8 @@
     <br><br>
     Thank you for completing your assessment.<br><br>
     We appreciate the time and effort you’ve invested in completing this.<br><br>
-    For further guidance and support visit our [support page](https://support.leadershipacademy.nhs.uk/).<br><br>
+    For further guidance and support visit our <a href="https://support.leadershipacademy.nhs.uk/">support page</a>.<br><br>
     Best regards,<br><br>
     Assessment System Team<br><br>
-    W: [leadershipacademy.nhs.uk](https://leadershipacademy.nhs.uk) | Follow us: @NHSLeadership
+    W: <a href="https://leadershipacademy.nhs.uk">leadershipacademy.nhs.uk</a> | Follow us: @NHSLeadership
 @endcomponent
--- resources/views/mail/rater-feedback-submitted.blade.php
+++ resources/views/mail/rater-feedback-submitted.blade.php
@@ -1,14 +1,12 @@
 @component('mail::message')
-    A rater has submitted their feedback for your assessment.
-    <br><br>
-    You can view the progress of your assessment using the button below.
-    <br><br>
+    A rater has submitted their feedback for your assessment.<br><br>
+    You can view the progress of your assessment using the button below.<br><br>
     @component('mail::button', ['url' => route('assessment-raters', ['assessmentId' => $assessment->id])])
         View assessment
     @endcomponent
     <br><br>
-    For further guidance and support visit our [support page](https://support.leadershipacademy.nhs.uk/).<br><br>
-    Best regards,<br><br>
-    Assessment System Team<br><br>
-    W: [leadershipacademy.nhs.uk](https://leadershipacademy.nhs.uk) | Follow us: @NHSLeadership
+    For further guidance and support visit our <a href="https://support.leadershipacademy.nhs.uk/">support page</a>.<br><br>
+        Best regards,<br><br>
+        Assessment System Team<br><br>
+        W: <a href="https://leadershipacademy.nhs.uk">leadershipacademy.nhs.uk</a> | Follow us: @NHSLeadership
 @endcomponent
