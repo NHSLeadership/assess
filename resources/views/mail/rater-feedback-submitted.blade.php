7c8dde8dfb7d0059e7515fa40d94bd21fe5237e3
Andrew Blane <andrew.blane@nhs.net>
2026-08-27 16:05:22 +0100

    [refs #fix-email-links] Use HTML links in emails

--- resources/views/mail/rater-feedback-submitted.blade.php
+++ resources/views/mail/rater-feedback-submitted.blade.php
@@ -7,8 +7,8 @@
         View assessment
     @endcomponent
     <br><br>
-    For further guidance and support visit our [support page](https://support.leadershipacademy.nhs.uk/).<br><br>
+    For further guidance and support visit our <a href="https://support.leadershipacademy.nhs.uk/">support page</a>.<br><br>
     Best regards,<br><br>
     Assessment System Team<br><br>
-    W: [leadershipacademy.nhs.uk](https://leadershipacademy.nhs.uk) | Follow us: @NHSLeadership
+    W: <a href="https://leadershipacademy.nhs.uk">leadershipacademy.nhs.uk</a> | Follow us: @NHSLeadership
 @endcomponent
