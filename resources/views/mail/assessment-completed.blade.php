7cf2773bceb15cf543b2cc5fbf2bd2989b0d6e77
Andrew Blane <andrew.blane@nhs.net>
2026-09-03 14:37:07 +0100

    [refs #DIG-8597] Mention raters in completion email

--- resources/views/mail/assessment-completed.blade.php
+++ resources/views/mail/assessment-completed.blade.php
@@ -5,7 +5,11 @@ Dear {{ $subject->name }},
 
 Thank you for completing your assessment.
 
-We appreciate the time and effort you’ve invested in completing this.
+We appreciate the time and effort you’ve invested in reflecting on your leadership practice.
+
+@if($assessment->raters()->exists())
+Your invited raters may continue to submit their feedback over the coming weeks.
+@endif
 
 For further guidance and support visit our
 [support page](https://support.leadershipacademy.nhs.uk/).
