0ba951bbf7f8e439e563afe97ee8257f5d2bf36e
Andrew Blane <andrew.blane@nhs.net>
2026-09-23 14:53:20 +0100

    [refs #DIG-8627] Update banner following feedback

--- resources/views/elements/banner.blade.php
+++ resources/views/elements/banner.blade.php
@@ -3,14 +3,16 @@
 @endphp
 
 @if($banner->title || $banner->body)
-    <div class="nhsuk-notification-banner nhsuk-notification-banner--success" data-module="nhsuk-notification-banner" role="alert" aria-labelledby="nhsuk-notification-banner-title" data-nhsuk-notification-banner-init="">
+    <div class="nhsuk-notification-banner" data-module="nhsuk-notification-banner" role="region" aria-labelledby="nhsuk-notification-banner-title">
         <div class="nhsuk-notification-banner__header">
             <h2 class="nhsuk-notification-banner__title" id="nhsuk-notification-banner-title">
                 {{ $banner->title }}
             </h2>
         </div>
         <div class="nhsuk-notification-banner__content banner-content-full-width">
-            {!! $banner->body !!}
+            <p class="nhsuk-notification-banner__heading">
+                {!! $banner->body !!}
+            </p>
         </div>
     </div>
 @endif
